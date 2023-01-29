a6187add93280dc4ce6ade770124402c8d57421a
Ammar Hassan <ammar@ammarhassan.co.uk>
2023-01-29 11:05:59 +0000

    wrapped form tag and update to script

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,10 @@
 const button = document.querySelector("button");
+const form = document.querySelector("form");
 const input = document.querySelectorAll("input");
 const arr = Array.from(input);
 
-button.addEventListener("click", ()=>{
+form.addEventListener("submit", (e)=>{
+    e.preventDefault();
     arr.forEach((e)=>{
         if(e.value == ''){
             e.style.border = "1px solid red";
