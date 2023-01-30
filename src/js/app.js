ea5da01816e5ab498b87a04c1c410607d120202b
Ammar Hassan <ammar@ammarhassan.co.uk>
2023-01-30 12:20:19 +0000

    fetch

--- src/js/app.js
+++ src/js/app.js
@@ -13,4 +13,23 @@ form.addEventListener("submit", (e)=>{
             e.style.border = "1px solid #7eb909";
         }
     })
+    const formData = new FormData(form);
+    for(let data of formData){
+        console.log(data[0], data[1])
+    }
+    fetch('http://httpbin.org/post', {
+        method: "POST",
+        body: formData
+    })
+    .then(res => {
+        if(!res.ok){
+            console.log('not success')
+            console.log(res)
+        }else{
+            console.log('ok')
+        }
+    })
+    .then((res)=>res.json())
+    .then(res => console.log(res))
+
 })
